da5b147e12b09cd03d6c609190ada2a6a4292059
AndersonMS <amsilva_@outlook.com>
2019-01-25 16:51:15 -0200

    Add files

---
+++ Consumer.cs
@@ -0,0 +1,67 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace TestConsumerRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                            queue: "QueueTeste",
+                            exclusive: false,
+                            durable: false,
+                            autoDelete: false,
+                            arguments: null
+                        );
+
+                    channel.QueueBind(
+                            queue: "QueueTeste",
+                            exchange: "ExchangeTeste",
+                            routingKey: "teste",
+                            arguments: null
+                        );
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        try
+                        {
+                            var body = ea.Body;
+                            var message = JsonConvert.DeserializeObject<TestObject>(Encoding.UTF8.GetString(body));
+                            Console.WriteLine(message.Nome);
+                            channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                        }
+                        catch(Exception ex)
+                        {
+                            channel.BasicNack(deliveryTag: ea.DeliveryTag, multiple: false, requeue: false);
+                        }
+                    };
+                    channel.BasicConsume(
+                            queue: "QueueTeste",
+                            autoAck: false,
+                            consumer: consumer
+                        );
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+    
+    public class TestObject
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ Publisher.cs
@@ -0,0 +1,77 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ProjetoTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(
+                            exchange: "ExchangeTeste",
+                            type: ExchangeType.Direct,//Ver para que isso serve
+                            durable: false,
+                            autoDelete: false,
+                            arguments: null
+                        );
+
+                    TestObject objeto = new TestObject() { Codigo = 1, Nome = "Teste" };
+                    var body = System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(objeto));
+
+                    channel.BasicPublish(
+                            exchange: "ExchangeTeste",
+                            routingKey: "teste",
+                            basicProperties: null,
+                            body: body
+                        );
+
+                    channel.BasicPublish(
+                            exchange: "ExchangeTeste",
+                            routingKey: "teste",
+                            basicProperties: null,
+                            body: body
+                        );
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+    
+    public class TestObject
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+
+    public static class ObjectExtension
+    {
+        public static byte[] ObjectToByteArray(this Object obj)
+        {
+            if (obj == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, obj);
+            return ms.ToArray();
+        }
+
+        public static Object ByteArrayToObject(this byte[] arrBytes)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryFormatter binForm = new BinaryFormatter();
+            memStream.Write(arrBytes, 0, arrBytes.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            Object obj = (Object)binForm.Deserialize(memStream);
+            return obj;
+        }
+    }
+}
